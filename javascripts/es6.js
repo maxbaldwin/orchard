c44584f6daabae9c2a2337f361513456a95a7c3c
Max Baldwin <maxrbaldwin@gmail.com>
2016-10-08 11:34:44 -0500

    added stuff

--- javascripts/es6.js
+++ javascripts/es6.js
@@ -1,104 +1,104 @@
-// console.log('es6');
-// //const
-// const a = 8; 
-// console.log('a', a); 
-// a=12;
-// console.log('a', a); 
-//const outputDiv = $('#output');
+// // console.log('es6');
+// // //const
+// // const a = 8; 
+// // console.log('a', a); 
+// // a=12;
+// // console.log('a', a); 
+// //const outputDiv = $('#output');
 
-//let
-// let b=8; 
-// console.log('b', b);
-// b=12; 
-// console.log('b2', b); 
+// //let
+// // let b=8; 
+// // console.log('b', b);
+// // b=12; 
+// // console.log('b2', b); 
 
-//difference between 'var' and 'let'
-// function varTest(){
-//   var x = 1; 
-//   if(true) {
-//     var x=2;
-//     console.log('inside if',x);
-//   }
-//   console.log('outsided if', x); 
+// //difference between 'var' and 'let'
+// // function varTest(){
+// //   var x = 1; 
+// //   if(true) {
+// //     var x=2;
+// //     console.log('inside if',x);
+// //   }
+// //   console.log('outsided if', x); 
+// // }
+
+// // varTest(); 
+
+// // function letTest(){
+// //   let y = 31; 
+// //   if(true) {
+// //     let y = 68;
+// //     console.log('inside if',y);
+// //   }
+// //   console.log('outsided if', y); 
+// // }
+
+// // letTest(); 
+
+// //arrow functions
+// var sum = function(){
+//   let a=1,b=2;
+//   return a+b; 
 // }
+// console.log("hardcoded sum in ES5", sum());
 
-// varTest(); 
-
-// function letTest(){
-//   let y = 31; 
-//   if(true) {
-//     let y = 68;
-//     console.log('inside if',y);
-//   }
-//   console.log('outsided if', y); 
+// var sum2 = () => {
+//   let a=1,b=2;
+//   return a+b; 
 // }
+// console.log("hardcoded sum in ES5", sum2());
 
-// letTest(); 
+// var reflect = function(value) {
+//   return value;
+// }
+// console.log("reflect in ES5", reflect('Es5 is so yesterday'));
 
-//arrow functions
-var sum = function(){
-  let a=1,b=2;
-  return a+b; 
-}
-console.log("hardcoded sum in ES5", sum());
+// var reflect2 = (value) => value; 
+// console.log("reflect in ES6", reflect2('Es6 is so today'));
 
-var sum2 = () => {
-  let a=1,b=2;
-  return a+b; 
-}
-console.log("hardcoded sum in ES5", sum2());
+// var realSum = function(num1, num2) {
+//   return num1+num2;
+// }
+// console.log("realsum ES5", realSum(2,3))
 
-var reflect = function(value) {
-  return value;
-}
-console.log("reflect in ES5", reflect('Es5 is so yesterday'));
+// var realSum2 = (num1, num2) => {
+//   return num1 + num2;
+// }
+// console.log("realsum ES6", realSum2(5,4))
 
-var reflect2 = (value) => value; 
-console.log("reflect in ES6", reflect2('Es6 is so today'));
+// //string templates
+// let m="bite me";
+// console.log('m',m); 
+// let r = `hello my name is ${m}`;
+// console.log('r', r); 
 
-var realSum = function(num1, num2) {
-  return num1+num2;
-}
-console.log("realsum ES5", realSum(2,3))
+// //object literal property shorthand
+// let wow='Hi there', es6="ES6", numNum = ()=>{console.log('howdy')}
 
-var realSum2 = (num1, num2) => {
-  return num1 + num2;
-}
-console.log("realsum ES6", realSum2(5,4))
+// let myOldObject = {
+//   wow:wow,
+//   es6:es6, 
+//   numNum:numNum
+// }
+// console.log("myOldObject", myOldObject); 
 
-//string templates
-let m="bite me";
-console.log('m',m); 
-let r = `hello my name is ${m}`;
-console.log('r', r); 
+// let myNewObject = {
+//   wow, es6, numNum
+// }
+// console.log('myOldObject', myNewObject); 
+// //method properties
 
-//object literal property shorthand
-let wow='Hi there', es6="ES6", numNum = ()=>{console.log('howdy')}
+// var e5Obj = {
+//   foo: function(){console.log('foo')},
+//   bar: function(){console.log('bar')}
+// }
+// console.log("e5Obj", e5Obj); 
 
-let myOldObject = {
-  wow:wow,
-  es6:es6, 
-  numNum:numNum
-}
-console.log("myOldObject", myOldObject); 
-
-let myNewObject = {
-  wow, es6, numNum
-}
-console.log('myOldObject', myNewObject); 
-//method properties
-
-var e5Obj = {
-  foo: function(){console.log('foo')},
-  bar: function(){console.log('bar')}
-}
-console.log("e5Obj", e5Obj); 
-
-var es6Obj = {
-  foo() {console.log('foo')}, 
-  bar() {console.log('bar')}
-}
-console.log("es6Obj", es6Obj);
+// var es6Obj = {
+//   foo() {console.log('foo')}, 
+//   bar() {console.log('bar')}
+// }
+// console.log("es6Obj", es6Obj);
 
 
 
